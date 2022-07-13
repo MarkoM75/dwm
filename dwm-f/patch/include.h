32bf4f3036a393a98645de9f24e1e2d204482e63
MarkoM75 <82847690+MarkoM75@users.noreply.github.com>
2022-07-13 14:10:31 +0000

    Add files via upload

---
+++ dwm-f/patch/include.h
@@ -0,0 +1,18 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "togglefullscreen.h"
+#include "zoomswap.h"
+/* Layouts */
+#include "layout_fibonacci.h"
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
